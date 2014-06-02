a0b888825d664c9b5b1ea46943a815cf2d258b2a
tytskiy <aleksandr.tyckij@gmail.com>
2014-06-02 22:17:54 +0300

    add miliseconds module

--- Helpers.js
+++ Helpers.js
@@ -303,6 +303,24 @@
         }
     };
 
+    // adopt https://github.com/henrikjoreteg/milliseconds
+    Helpers.prototype.ms = (function () {
+        var calc;
+
+        calc = function (m) {
+            return function(n) { return Math.round(n * m); };
+        };
+        return {
+            seconds: calc(1e3),
+            minutes: calc(6e4),
+            hours: calc(36e5),
+            days: calc(864e5),
+            weeks: calc(6048e5),
+            months: calc(26298e5),
+            years: calc(315576e5)
+        };
+    })();
+
     Helpers.prototype.extend = function () {
         return Helpers.extend.apply(arguments);
     };
